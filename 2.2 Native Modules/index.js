e4e1041d71b4e0872027786802ef0fa2e31bf9e4
Mayur Yaduvanshi <singhmayurraj97@gmail.com>
2023-11-01 21:55:52 +0530

    Let's learn Backend

--- 2.2 Native Modules/index.js
+++ 2.2 Native Modules/index.js
@@ -1,9 +1,11 @@
 const fs  = require("fs");
-fs.writeFile("trail.java" , "That's how we do it" , (err)=>{
+
+fs.writeFile("trail.txt" , "That's how we do it" , (err)=>{
     if (err) throw err;
     console.log("The file has been saved");
 });
-fs.readFile('trail.txt' , (err , data)=>{
+
+fs.readFile("trail.txt" , "utf8", (err , data)=>{
     if(err) throw err;
     console.log(data);
 })
--- 2.2 Native Modules/readFile.js
+++ 2.2 Native Modules/readFile.js
@@ -1,5 +1,5 @@
 const fsread = require("fs");
-fsread.readFile('trail.txt' , (err , data)=>{
+fsread.readFile('trail.txt' ,"utf8", (err , data)=>{
     if(err) throw err;
     console.log(data);
 })
