31e1cda3849edc96c7e130af51c129983230069c
Mayur Yaduvanshi <singhmayurraj97@gmail.com>
2023-11-01 21:46:59 +0530

    Personal repo

--- 2.2 Native Modules/index.js
+++ 2.2 Native Modules/index.js
@@ -3,3 +3,7 @@ fs.writeFile("trail.java" , "That's how we do it" , (err)=>{
     if (err) throw err;
     console.log("The file has been saved");
 });
+fs.readFile('trail.txt' , (err , data)=>{
+    if(err) throw err;
+    console.log(data);
+})
