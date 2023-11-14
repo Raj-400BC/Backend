878a102589a0f3f0f9e78b384649e1b6758b7fb2
Mayur Yaduvanshi <singhmayurraj97@gmail.com>
2023-11-14 21:41:53 +0530

    Learning how API fetching works .

--- 5.2 JSON/index.js
+++ 5.2 JSON/index.js
@@ -40,3 +40,12 @@ app.post("/recipe", (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
 });
+
+  // Step 2: Play around with the drop downs and see what gets logged.
+  // Use axios to make an API request to the /filter endpoint. Making
+  // sure you're passing both the type and participants queries.
+  // Render the index.ejs file with a single *random* activity that comes back
+  // from the API request.
+  // Step 3: If you get a 404 error (resource not found) from the API request.
+  // Pass an error to the index.ejs to tell the user:
+  // "No activities that match your criteria."
