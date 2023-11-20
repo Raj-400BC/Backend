c6d9c9c0765f949ba63b35af326275d9e8430931
Mayur Yaduvanshi <singhmayurraj97@gmail.com>
2023-11-20 17:14:00 +0530

    Learning About Authentication

--- 5.3 Axios/index.js
+++ 5.3 Axios/index.js
@@ -30,19 +30,27 @@ app.get("/", async (req, res) => {
   }
 });
 
-app.post("/", async (req, res) => {
-  
-  // console.log(result.data.headers['Content-Type']);
-res.redirect('/');
 
-  // Step 2: Play around with the drop downs and see what gets logged.
-  // Use axios to make an API request to the /filter endpoint. Making
-  // sure you're passing both the type and participants queries.
-  // Render the index.ejs file with a single *random* activity that comes back
-  // from the API request.
-  // Step 3: If you get a 404 error (resource not found) from the API request.
-  // Pass an error to the index.ejs to tell the user:
-  // "No activities that match your criteria."
+app.post("/", async (req, res) => {
+  try {
+    console.log(req.body);
+    const type = req.body.type;
+    const participants = req.body.participants;
+    const price = req.body.price;
+    const response = await axios.get(
+      `https://bored-api.appbrewery.com/filter?type=${type}&participants=${participants}`
+    );
+    const result = response.data;
+    console.log(result);
+    res.render("index.ejs", {
+      data: result[Math.floor(Math.random() * result.length)],
+    });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: "No activities that match your criteria.",
+    });
+  }
 });
 
 app.listen(port, () => {
